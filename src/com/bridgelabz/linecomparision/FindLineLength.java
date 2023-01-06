a80794517f0618d64a63a9dbb7a1499b38c21ce9
komalarao <manoharvakamullu1999@gmail.com>
2023-01-06 07:10:41 -0800

    added: welcome message to line comparision

---
+++ src/com/bridgelabz/linecomparision/FindLineLength.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linecomparision;
+
+public class FindLineLength {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line comparision program");
+
+	}
+
+}
